eced7b06796a3eefe08e2b995c83398315c60760
Momin Riyadh <mominriyadh@gmail.com>
2022-05-22 15:10:30 +0600

    fix and deploy

--- config/db.js
+++ config/db.js
@@ -15,4 +15,3 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require ('./config/db');
 const app = express();
 
 //Connect Datbase
-connectDB();
+connectDB;
 
 app.get("/", (req, res) => res.send('API RUNNING'));
 
